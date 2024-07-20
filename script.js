4bdb1b2bf7a7acca9ab4531f5f10552bc347f0ae
Leonid3352 <157629786+Leonid3352@users.noreply.github.com>
2024-07-20 23:19:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-let swiper = new Swiper('.mySwiper', {
-    spaceBetween:30,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: '.swiper-button-prev'
-    },
-    pagination: {
-        el: ".swiper-pagination",
-    },
-    mousewhell: true,
-    keyboard: true
-});
+const images = document.querySelectorAll('.slider-img');
+const controlls = document.querySelectorAll('.controlls');
+let imagesIndex = 0;
+
+function show(index) {
+    images[imagesIndex].classList.remove('active');
+    images[index].classList.add('active')
+    imagesIndex = index
+}
+
+controlls.forEach((e) => {
+    e.addEventListener('click', () =>{
+        if (event.target.classList.contains('prev')) {
+            let index = imagesIndex - 1;
+
+            if (index < 0) {
+                index = images.length - 1;
+            }
+            show(index);
+            
+        } else if (event.target.classList.contains('next')) {
+            let index = imagesIndex + 1;
+            if (index >= images.length) {
+                index = 0;
+            }
+            show(index)
+        }   
+    })
+})
+
+show(imagesIndex)
